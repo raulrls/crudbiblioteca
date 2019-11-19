762a1465d9bb8f30cb158411558a08bea606f3da
Alienware <raul.rosales@hotmail.com>
2019-11-19 14:39:39 -0700

    Solution
    .sln files

---
+++ WebApp-Biblioteca/WebApp-Biblioteca/Controllers/AdministradorController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp_Biblioteca.Models.EntidadesBiblioteca;
+
+
+namespace WebApp_Biblioteca.Controllers
+{
+    public class AdministradorController : Controller
+    {
+        // GET: Login
+        public ActionResult IniciarSesion() {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult IniciarSesion(Administrador admin) {
+            using (Entities dbcon = new Entities())
+            {
+                var nombre = dbcon.SP_login_administrador(admin.correo, admin.contrasena).ToList();
+                if (nombre.Count != 0)
+                {
+                    return Redirect("Menu");
+                }
+                else
+                {
+                    return Redirect("IniciarSesion");
+                }
+                
+            }
+        }
+
+        public ActionResult Menu() {
+            return View();
+        }
+
+
+    }
+}
---
+++ WebApp-Biblioteca/WebApp-Biblioteca/Controllers/TipoPrestamoController.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApp_Biblioteca.Models.EntidadesBiblioteca;
+
+namespace WebApp_Biblioteca.Controllers
+{
+    public class TipoPrestamoController : Controller
+    {
+        // GET: TipoPrestamo
+        public ActionResult ListaTipoPrestamos()
+        {
+            using (Entities dbcon = new Entities())
+            {
+                var rawData = dbcon.SP_S_tipoPrestamo().ToList();
+                return View(rawData);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult AgregarTipoPrestamo() {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult AgregarTipoPrestamo(TipoPrestamo tipoPrestamo)
+        {
+            using (Entities dbcon = new Entities())
+            {
+                dbcon.SP_I_tipoPrestamo(tipoPrestamo.nombrePrestamo, tipoPrestamo.descripcion);
+                return Redirect("ListaTipoPrestamos");
+            }
+        }
+
+        [HttpGet]
+        public ActionResult BorrarTipoPrestamo(int idTipoPrestamo)
+        {
+            using (Entities dbcon = new Entities())
+            {
+                TipoPrestamo tipoPrestamo = new TipoPrestamo();
+                tipoPrestamo = dbcon.TipoPrestamoes.Where(x => x.idTipoPrestamo == idTipoPrestamo).FirstOrDefault();
+                return View(tipoPrestamo);
+            }
+        }
+
+        [HttpPost]
+        public ActionResult BorrarTipoPrestamo(TipoPrestamo tipoPrestamo)
+        {
+            using (Entities dbcon = new Entities())
+            {
+                dbcon.SP_D_tipoPrestamo(tipoPrestamo.idTipoPrestamo);
+                return Redirect("ListaTipoPrestamos");
+            }
+        }
+
+    }
+}
